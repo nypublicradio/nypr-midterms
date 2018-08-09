75bed41528b6414e39b162841e45a2151ba5d602
Michael Hearn <git@mikehearn.net>
2018-08-09 13:39:14 -0400

    Update to use changesets

--- app/components/opt-in/component.js
+++ app/components/opt-in/component.js
@@ -1,17 +1,26 @@
-import Component from '@ember/component';
-import { validate } from 'ember-validators';
-import { computed } from '@ember/object';
+import Changeset from "ember-changeset";
+import Component from "@ember/component";
+import lookupValidator from "ember-changeset-validations";
+import { set } from "@ember/object";
+import { validateFormat } from "ember-changeset-validations/validators";
+
+let validations = {
+  email: validateFormat({ type: "email", allowBlank: true }),
+  phone: validateFormat({ type: "phone", allowBlank: true })
+};
 
 export default Component.extend({
   init() {
-    this._super(...arguments)
-  },
-  optinEmail: null,
-  optinPhone: null,
-  optinEmailValid: computed('optinEmail', function() {
-    return validate('format', this.get('optinEmail'), {type: 'email', message: 'asodifj'})
-  }),
-  optinPhoneValid: computed('optinPhone', function() {
-    return validate('format', this.get('optinPhone'), {type: 'phone'})
-  })
+    this._super(...arguments);
+    let obj = {
+      email: null,
+      phone: null
+    };
+    this.changeset = new Changeset(
+      obj,
+      lookupValidator(validations),
+      validations
+    );
+    set(this, "changeset", this.changeset);
+  }
 });
