f594ad4b8c77ef2e72d6807741e7bfc7a4eda8ed
Michael Hearn <git@mikehearn.net>
2018-08-09 07:49:48 -0400

    Partial validation

--- app/components/opt-in/component.js
+++ app/components/opt-in/component.js
@@ -1,9 +1,17 @@
 import Component from '@ember/component';
+import { validate } from 'ember-validators';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   init() {
     this._super(...arguments)
   },
   optinEmail: null,
-  optinPhone: null
+  optinPhone: null,
+  optinEmailValid: computed('optinEmail', function() {
+    return validate('format', this.get('optinEmail'), {type: 'email', message: 'asodifj'})
+  }),
+  optinPhoneValid: computed('optinPhone', function() {
+    return validate('format', this.get('optinPhone'), {type: 'phone'})
+  })
 });
