a78dd8778e8b60636a59041c224ca17820ed0e54
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2018-08-09 10:17:34 -0400

    get chunks api working, build out hearken modules

--- app/adapters/chunk.js
+++ app/adapters/chunk.js
@@ -1,11 +1,4 @@
 import ChunkAdapter from 'nypr-publisher-lib/adapters/chunk';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default ChunkAdapter.extend(DataAdapterMixin, {
-  authorize(xhr) {
-    let headers = this.get('session').authorize({});
-    for (var h in headers) {
-      xhr.setRequestHeader(h, headers[h]);
-    }
-  },
+export default ChunkAdapter.extend({
 });
--- app/components/hearken-module.js
+++ app/components/hearken-module.js
@@ -1,4 +1,27 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
 
 export default Component.extend({
+  store: service(),
+  chunk: null,
+
+  init(){
+    this._super(...arguments);
+    console.log(this.get('slug'))
+    this._getChunk();
+  },
+
+  _getChunk() {
+    this.get('store').findRecord('chunk', this.get('slug')).then((chunk) => {
+
+      // because this is async you need to guard against the component
+      // being destroyed before the api call has finished because
+      // because `this.set` will throw an error.
+      if (this.isDestroyed) { return; }
+
+      this.set('chunk', chunk);
+      console.log(this.get('chunk'));
+    })
+  }
 });
