3a7aa78230797012a64c0e618158bf489fad20d3
Brian Whitton <me@brianwhitton.com>
2018-08-10 10:19:23 -0400

    update episode tease component test

--- tests/integration/components/episode-tease/component-test.js
+++ tests/integration/components/episode-tease/component-test.js
@@ -1,23 +1,51 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+
+import test from 'ember-sinon-qunit/test-support/test';
 
 module('Integration | Component | episode-tease', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  test('usage', async function(assert) {
+    let dj = this.owner.lookup('service:dj');
+    this.mock(dj).expects('play').withArgs('audio-id').resolves();
+
+    this.set('links', [{
+      title: 'via RSS',
+      href: 'http://rss.com'
+    }, {
+      title: 'via iTunes',
+      href: 'http://itunes.com'
+    }]);
+
     await render(hbs`
       {{#episode-tease audio='audio-id' as |tease|}}
-        {{#tease.content}}
-        {{/tease.content}}
+        {{tease.title "This Great Episode"}}
+        {{tease.link "See All Episodes" "http://moreepisodes.com"}}
 
-        {{#tease.footer}}
-        {{/tease.footer}}
+        {{#tease.body}}
+          Lorem ipsum lorem ipsum
+        {{/tease.body}}
+
+        {{tease.footer links=links}}
 
       {{/episode-tease}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('article.episode-tease').exists();
+    assert.dom('.episode-tease__title').hasText('This Great Episode');
+
+    let moreLink = assert.dom('.episode-tease__link')
+    moreLink.hasText('See All Episodes');
+    moreLink.hasAttribute('href', 'http://moreepisodes.com');
+
+    assert.dom('.episode-tease__body').hasText('Lorem ipsum lorem ipsum');
+
+    assert.dom('.episode-subscribe li').exists({count: 2});
+    assert.dom('[data-test-subscribe="0"] .fa-rss').exists('should use an rss icon for rss links');
+
+    await click('[data-test-listen]');
   });
 });
