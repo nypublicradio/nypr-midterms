1d34232187ac5ef50a9952a5994f39bad2e84260
Brian Whitton <me@brianwhitton.com>
2018-08-13 10:25:39 -0400

    add types to gothamist model attributes

--- app/models/gothamist-story.js
+++ app/models/gothamist-story.js
@@ -4,13 +4,13 @@ import { computed } from '@ember/object';
 import moment from 'moment';
 
 export default DS.Model.extend({
-  author: DS.attr(),
-  authoredOn: DS.attr(),
   imageMain: DS.attr(),
+  author: DS.attr('string'),
+  authoredOn: DS.attr('string'),
   producingOrganizations: 'Gothamist',
-  tease: DS.attr(),
-  title: DS.attr(),
-  url: DS.attr(),
+  tease: DS.attr('string'),
+  title: DS.attr('string'),
+  url: DS.attr('string'),
 
   newsdate: computed('authoredOn', function() {
     return moment(this.authoredOn,  'YYYYMMDDHHmmss', 'America/New_York');
