1a2af017871fe0081e2c678e4bec7b71e87111cb
Brian Whitton <me@brianwhitton.com>
2018-08-13 11:02:07 -0400

    make this safer if nothing's passed in

--- app/components/read-section/component.js
+++ app/components/read-section/component.js
@@ -6,7 +6,9 @@ export default Component.extend({
   tagName: 'section',
   classNames: ['read-section'],
   sorted: computed('wnyc', 'gothamist', function() {
-    let stories = this.wnyc.toArray().concat(this.gothamist.toArray());
+    let wnyc = this.wnyc || [];
+    let gothamist = this.gothamist || [];
+    let stories = wnyc.toArray().concat(gothamist.toArray());
     return stories.sort((a, b) => moment(b.newsdate) - moment(a.newsdate));
   })
 });
