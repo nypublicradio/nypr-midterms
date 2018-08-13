df4c1a84926c646172e73db790515cda1f8f4032
Brian Whitton <me@brianwhitton.com>
2018-08-13 11:59:31 -0400

    adds gothamist adapter test

--- tests/unit/adapters/gothamist-story-test.js
+++ tests/unit/adapters/gothamist-story-test.js
@@ -1,5 +1,10 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupTest } from 'ember-qunit';
+
+import test from 'ember-sinon-qunit/test-support/test';
+import * as fetch from 'fetch';
+
+import config from '../../../config/environment';
 
 module('Unit | Adapter | gothamist story', function(hooks) {
   setupTest(hooks);
@@ -8,5 +13,21 @@ module('Unit | Adapter | gothamist story', function(hooks) {
   test('it exists', function(assert) {
     let adapter = this.owner.lookup('adapter:gothamist-story');
     assert.ok(adapter);
+  });
+
+  test('it requests the gothamist endpoint for stories for a query', function() {
+    const QUERY = {
+      tag: 'foo',
+      limit: 2
+    };
+
+    this.mock(fetch)
+      .expects('default')
+      .once()
+      .withArgs(`${config.gothamistAPI}?index=gothamist&tag=${QUERY.tag}`)
+      .resolves({ json: () => Promise.resolves(['foo', 'bar', 'baz']) });
+
+    let adapter = this.owner.lookup('adapter:gothamist-story');
+    adapter.query(null, null, QUERY);
   });
 });
