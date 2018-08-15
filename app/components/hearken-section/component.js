64eb1258b5ce75daf7d3aac87861f6cbe26e1ab0
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2018-08-15 15:22:38 -0400

    create styles, add second chunk

--- app/components/hearken-section/component.js
+++ app/components/hearken-section/component.js
@@ -9,20 +9,19 @@ export default Component.extend({
 
   init(){
     this._super(...arguments);
-    console.log(this.get('slug'))
-    this._getChunk();
+    this._getChunk(this.get('slug1'), "chunk1");
+    this._getChunk(this.get('slug2'), "chunk2");
   },
 
-  _getChunk() {
-    this.get('store').findRecord('chunk', this.get('slug')).then((chunk) => {
+  _getChunk(slug, chunkName) {
+    this.get('store').findRecord('chunk', slug).then((chunk) => {
 
       // because this is async you need to guard against the component
       // being destroyed before the api call has finished because
       // because `this.set` will throw an error.
       if (this.isDestroyed) { return; }
 
-      this.set('chunk', chunk);
-      console.log(this.get('chunk'));
+      this.set(chunkName, chunk);
     })
   }
 });
