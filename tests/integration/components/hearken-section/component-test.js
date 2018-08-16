6773cb6a2e324a6f95de04c21daa0e8bfda7905a
Bryn Scharenberg <bscharenberg@nypublicradio.org>
2018-08-16 11:38:59 -0400

    write and fix tests

--- tests/integration/components/hearken-section/component-test.js
+++ tests/integration/components/hearken-section/component-test.js
@@ -1,26 +1,43 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, findAll, find, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+
 
 module('Integration | Component | hearken-section', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  test('it renders', async function(assert) {
+  hooks.beforeEach(function() {
+    let chunk1 = this.server.create('chunk', {slug: 'midterms-hearken-chunk-1'});
+    this.server.db.chunks.update(chunk1.id, {content: '<a href="http://foo.com" class="foo1">foo</a>'});
+    this.chunk1 = chunk1;
+    let chunk2 = this.server.create('chunk', {slug: 'midterms-hearken-chunk-2'});
+    this.server.db.chunks.update(chunk2.id, {content: '<a href="http://foo.com" class="foo2">foofoo</a>'});
+    this.chunk2 = chunk2;
+  });
+
+
+  test('it renders only tease when closed (default state)', async function(assert) {
+// Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`{{hearken-section slug1='midterms-hearken-chunk-1' slug2='midterms-hearken-chunk-2'}}`);
+    assert.equal(findAll('.foo1').length, 1);
+    assert.equal(find('.foo1').textContent.trim(), "foo");
+    assert.equal(findAll('.foo2').length, 0);
+  });
+
+  test('it renders tease and body when toggeled open', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{hearken-section}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#hearken-section}}
-        template block text
-      {{/hearken-section}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    await render(hbs`{{hearken-section slug1='midterms-hearken-chunk-1' slug2='midterms-hearken-chunk-2'}}`);
+    await click('.page-section__toggle');
+    assert.equal(findAll('.foo1').length, 1);
+    assert.equal(find('.foo1').textContent.trim(), "foo");
+    assert.equal(findAll('.foo2').length, 1);
+    assert.equal(find('.foo2').textContent.trim(), "foofoo");
   });
 });
