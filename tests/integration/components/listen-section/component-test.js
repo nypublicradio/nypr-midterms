4bdc1bfc2ae69f25920b589c7d07c4990d32e1a2
Brian Whitton <me@brianwhitton.com>
2018-08-20 14:33:06 -0400

    update this test

--- tests/integration/components/listen-section/component-test.js
+++ tests/integration/components/listen-section/component-test.js
@@ -13,19 +13,13 @@ module('Integration | Component | listen-section', function(hooks) {
     assert.dom('.listen-section').exists();
   });
 
-  test('it hides the buttons when open', async function(assert) {
-    await render(hbs`{{listen-section}}`);
-
-    await click('[data-test-open-listen]');
-    assert.dom('.listen-section > .page-section').hasClass('is-open');
-  });
-
   test('it plays the wnyc live stream', async function() {
     let dj = this.owner.lookup('service:dj');
-    this.mock(dj).expects('play').withArgs('wnyc-fm939');
+    this.mock(dj).expects('play').withArgs('wnyc-fm939').resolves();
 
     await render(hbs`{{listen-section}}`);
+    await click('[data-test-open-section]');
 
-    await click('[data-test-listen-live]');
+    await click('[data-test-live-stream] [data-test-listen]');
   });
 });
