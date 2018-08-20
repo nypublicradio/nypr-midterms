75efe175c76e0188b5097ecd1cc3dc8fc92218cd
Brian Whitton <me@brianwhitton.com>
2018-08-20 17:14:44 -0400

    make this safer

--- app/instance-initializers/hash-scroll.js
+++ app/instance-initializers/hash-scroll.js
@@ -2,9 +2,11 @@ import { schedule } from '@ember/runloop';
 
 export function initialize(appInstance) {
   if (typeof location !== 'undefined' && location.hash) {
-    let scroll = appInstance.lookup('service:scroller');
-    let options = {offset: -100};
-    schedule('afterRender', () => scroll.scrollVertical(location.hash, options));
+    if (document.querySelector(location.hash)) {
+      let scroll = appInstance.lookup('service:scroller');
+      let options = {offset: -100};
+      schedule('afterRender', () => scroll.scrollVertical(location.hash, options));
+    }
   }
 }
 
