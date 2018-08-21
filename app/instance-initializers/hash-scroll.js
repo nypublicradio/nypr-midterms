054202178ea16086b449784a608c3e445ba5c51f
Brian Whitton <me@brianwhitton.com>
2018-08-21 12:26:58 -0400

    add comment

--- app/instance-initializers/hash-scroll.js
+++ app/instance-initializers/hash-scroll.js
@@ -2,7 +2,7 @@ import { schedule } from '@ember/runloop';
 
 export function initialize(appInstance) {
   if (typeof location !== 'undefined' && location.hash) {
-    if (document.querySelector(location.hash)) {
+    if (document.querySelector(location.hash)) { // don't scroll to an ID if it doesn't exist
       let scroll = appInstance.lookup('service:scroller');
       let options = {offset: -100};
       schedule('afterRender', () => scroll.scrollVertical(location.hash, options));
