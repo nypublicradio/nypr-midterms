3027e3754a758f3d6a4772fc6fdcaace9612a58f
Brian Whitton <me@brianwhitton.com>
2018-08-22 17:44:20 -0400

    reset scroll on transition
    also don't mess with the data layer in fastboot land

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
 import { schedule } from '@ember/runloop';
+import { inject } from '@ember/service';
 
 export default Route.extend({
+  fastboot: inject(),
+
   title(tokens) {
     tokens.unshift('WNYC + Gothamist');
     tokens.unshift('Your Voice Your Choice 2018');
@@ -9,13 +12,16 @@ export default Route.extend({
   },
   actions: {
     didTransition() {
-      schedule('afterRender', () => {
-        if (window.dataLayer) {
-          window.dataLayer.push({
-            event: 'Page View'
-          });
-        }
-      });
+      if (!this.fastboot.isFastBoot)  {
+        schedule('afterRender', () => {
+          if (window.dataLayer) {
+            window.dataLayer.push({
+              event: 'Page View'
+            });
+          }
+          window.scrollTo(0, 0);
+        });
+      }
     }
   }
 })
