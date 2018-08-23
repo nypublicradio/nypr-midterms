e112ab17b63792b42d859c8b02acf4a4e94e287a
Brian Whitton <me@brianwhitton.com>
2018-08-23 13:47:48 -0400

    rewrite these tests so they don't rely on mirage
    - assert that `store.findRecord` is called with the passed in chunk slug 
    and specify that it resolves with a chunk that has the expect attribute
    - make content a script element so that we can assert that the script is 
    executed in this context
    - assert that the expected text is inserted into the expected dom 
    element, based on the script

--- tests/integration/components/hearken-section/component-test.js
+++ tests/integration/components/hearken-section/component-test.js
@@ -1,29 +1,34 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, findAll } from '@ember/test-helpers';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
-import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
-
+import test from 'ember-sinon-qunit/test-support/test';
 
 module('Integration | Component | hearken-section', function(hooks) {
   setupRenderingTest(hooks);
-  setupMirage(hooks);
-
-  hooks.beforeEach(function() {
-    let chunk2 = this.server.create('chunk', {slug: 'midterms-hearken-chunk-2'});
-    this.server.db.chunks.update(chunk2.id, {content: '<a href="http://foo.com" class="foo2">foofoo</a>'});
-    this.chunk2 = chunk2;
-  });
-
-  hooks.afterEach(function() {
-      server.shutdown();
-  });
 
   test('it renders with the html from the chunk', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    const SCRIPT = `<script>
+      var p = document.createElement('p');
+      p.textContent = 'Hello World';
+      p.id = 'text';
+      document.querySelector('#hearken-section__wrapper').appendChild(p);
+    </script>`;
+    const SLUG = 'midterms-hearken-chunk-2';
+    const STORE = {
+      findRecord: this.mock()
+        .withArgs('chunk', SLUG)
+        .resolves({
+          content: SCRIPT
+        })
+    };
+    this.setProperties({
+      slug: SLUG,
+      store: STORE,
+    })
 
-    await render(hbs`{{hearken-section slug='midterms-hearken-chunk-2'}}`);
-    assert.equal(findAll('.foo2').length, 1);
+    await render(hbs`{{hearken-section slug=slug store=store}}`);
+
+    assert.dom('#text').hasText('Hello World');
   });
 });
