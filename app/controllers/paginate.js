fe1472bced64eef6e57a26159bde76fe01ed7644
Brian Whitton <me@brianwhitton.com>
2018-08-23 13:28:23 -0400

    final clean ups
    removes lingering paginate controller
    remove extra scss imports
    remove lingering lib directory
    uninstall broccoli concat

--- app/controllers/paginate.js
+++
@@ -1,8 +0,0 @@
-import Controller from '@ember/controller';
-
-export default Controller.extend({
-  queryParams: ['page', 'wnyctag', 'gothamisttag'],
-  page: 1,
-  wnyctag: 'news',
-  gothamisttag: '@wnyc'
-});
