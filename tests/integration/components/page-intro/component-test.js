3087ccdb8956ef3d0aedbf8de7b4c60f5f8718a2
Brian Whitton <me@brianwhitton.com>
2018-08-23 13:58:27 -0400

    fix this test

--- tests/integration/components/page-intro/component-test.js
+++ tests/integration/components/page-intro/component-test.js
@@ -1,12 +1,16 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import test from 'ember-sinon-qunit/test-support/test';
 
 module('Integration | Component | page-intro', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
+    let router = this.owner.lookup('service:router');
+    this.stub(router, 'urlFor').returns('');
+    
     await render(hbs`{{page-intro}}`);
 
     assert.dom('.page-intro').exists();
