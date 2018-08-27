e30d3f6adc1dfae79ac9418328f0b461161f5d5c
Brian Whitton <me@brianwhitton.com>
2018-08-27 14:14:02 -0400

    update ember responsive breakpoints to match sass breakpoints

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -3,6 +3,6 @@ import nyprPlayerBreakpoints from 'nypr-player/breakpoints';
 export default Object.assign({
   xSmall:  '(max-width: 359px)',
   small:   '(min-width: 360px)',
-  medium:  '(min-width: 1024px)',
+  medium:  '(min-width: 768px)',
   large:   '(min-width: 1440px)'
 }, nyprPlayerBreakpoints);
