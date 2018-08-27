7833ac551b96e67053835aa9e1576fca261535db
Brian Whitton <me@brianwhitton.com>
2018-08-27 19:46:11 -0400

    add margin below date on featured story teases w/o images on widescreens

--- app/components/story-tease/component.js
+++ app/components/story-tease/component.js
@@ -3,5 +3,5 @@ import Component from '@ember/component';
 export default Component.extend({
   tagName: 'article',
   classNames: ['story-tease'],
-  classNameBindings: ['featured:is-featured', 'alignment']
+  classNameBindings: ['featured:is-featured', 'alignment', 'imageUrl:has-image:no-image']
 });
