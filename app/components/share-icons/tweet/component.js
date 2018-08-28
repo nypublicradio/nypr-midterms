25898eb00fca84268dfc2b81081254956e4f819d
Michael Hearn <git@mikehearn.net>
2018-08-28 19:44:32 -0400

    URL Encode share text in Twitter/Email links

--- app/components/share-icons/tweet/component.js
+++ app/components/share-icons/tweet/component.js
@@ -5,7 +5,7 @@ export default Component.extend({
 
   urlToShare: function() {
     let url = encodeURIComponent(`${location}?utm_medium=${this.medium}&utm_campaign=${this.campaign}&utm_source=tw`);
-    let params = `text=${this.text || document.title}&url=${url}`;
+    let params = `text=${this.text || encodeURIComponent(document.title)}&url=${url}`;
     params = this.via ? `${params}&via=${this.via}` : params;
     return `https://twitter.com/intent/tweet?${params}`;
   },
