43e617b487490e5a726f2cd4a9fa7f6bc05d999a
Brian Whitton <me@brianwhitton.com>
2018-08-28 15:40:29 -0400

    add meta description

--- app/routes/hearken-hub.js
+++ app/routes/hearken-hub.js
@@ -5,9 +5,12 @@ import { hash } from 'rsvp';
 const WNYC_QUESTION_TAG = 'news';
 const GOTHAMIST_QUESTION_TAG = '@wnyc';
 
+const META_DESCRIPTION = "Do you have a question about how you can make a difference in your neighborhood, city or state? Is there anything you’d like to know about voting, the elections or how to navigate civic life in New York City? Ask us! Our reporters want to help you get involved by answering your questions.";
+
 export default Route.extend({
   titleToken: "Ask a Reporter",
 
+  head: service('head-data'),
   store: service(),
 
   classNames: ['hearken-route'],
@@ -34,4 +37,8 @@ export default Route.extend({
       })
     });
   },
+
+  afterModel() {
+    this.head.set('description', META_DESCRIPTION);
+  }
 });
