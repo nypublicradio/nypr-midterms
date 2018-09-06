d9002dc92afa759a7a322628e56776d59ad90efa
Michael Hearn <git@mikehearn.net>
2018-09-06 19:47:39 -0400

    Fix eslint import error

--- app/components/voter-guide-iframe/component.js
+++ app/components/voter-guide-iframe/component.js
@@ -1,7 +1,6 @@
 import Component from '@ember/component';
 import config from "../../config/environment";
 import pym from "pym";
-import { reads } from '@ember/object/computed';
 
 export default Component.extend({
   voterGuideIframe: config.voterGuideIframe,
