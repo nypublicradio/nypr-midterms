76c6bdfaec769891884830e1c834ebf198b50610
Michael Hearn <git@mikehearn.net>
2018-10-18 15:44:58 -0400

    Remove primary from meta description on voter guide

--- app/routes/voter-guide.js
+++ app/routes/voter-guide.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import config from '../config/environment';
 
-const META_DESCRIPTION = "WNYC, Gotham Gazette and City Limits analyze key races in this year’s New York State primary election";
+const META_DESCRIPTION = "WNYC, Gotham Gazette, City Limits and NJ Spotlight analyze key races in this year's New York and New Jersey midterm elections";
 
 export default Route.extend({
   titleToken: "2018 General Election Voter Guide",
