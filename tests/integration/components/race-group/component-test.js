c8cdec6595f2832b5e6bfbfdb00c46aaade51966
Brian Whitton <me@brianwhitton.com>
2018-10-18 16:55:16 -0400

    race group component for showing all races within a defined gropu

---
+++ tests/integration/components/race-group/component-test.js
@@ -0,0 +1,19 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { RACE_GROUP } from '../../../fixtures';
+
+module('Integration | Component | race-group', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function(assert) {
+    this.set('group', RACE_GROUP);
+
+    await render(hbs`{{race-group group=group}}`);
+
+    assert.dom('.race-group').exists();
+    assert.dom('.race-group__title').hasText(RACE_GROUP.title);
+    assert.dom('.race-detail').exists({count: RACE_GROUP.races.length});
+  });
+});
