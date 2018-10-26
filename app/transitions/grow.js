a33a04dd4ec8beed72c35e8c326cdb96f05b5a28
Brian Whitton <me@brianwhitton.com>
2018-10-26 13:32:26 -0400

    increase speed and decrease scale of grow animation

--- app/transitions/grow.js
+++ app/transitions/grow.js
@@ -2,10 +2,14 @@ import {
   animate,
 } from 'liquid-fire';
 
+const OPS = {
+  duration: 200
+};
+
 export default function grow() {
   this.lookup('crossFade').call(this, {duration: 10}).then(() => {
-    return animate(this.newElement, {scaleX: 1.4, scaleY: 1.4}).then(() => {
-      return animate(this.newElement, {scaleX: 1, scaleY: 1});
+    return animate(this.newElement, {scaleX: 1.2, scaleY: 1.2}, OPS).then(() => {
+      return animate(this.newElement, {scaleX: 1, scaleY: 1}, OPS);
     })
   });
 }
